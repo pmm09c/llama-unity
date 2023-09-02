764b813e934ddfb58ddfa732dbce07cd8aed728f
Peter morales <peter@mintstatelabs.com>
2023-09-02 16:53:00 -0700

    add whisper

---
+++ Assets/Samples/5 - Streaming/StreamingSampleMic.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Whisper.Utils;
+
+namespace Whisper.Samples
+{
+    /// <summary>
+    /// Stream transcription from microphone input.
+    /// </summary>
+    public class StreamingSampleMic : MonoBehaviour
+    {
+        public WhisperManager whisper;
+        public MicrophoneRecord microphoneRecord;
+    
+        [Header("UI")] 
+        public Button button;
+        public Text buttonText;
+        public Text text;
+        public ScrollRect scroll;
+        private WhisperStream _stream;
+
+        private async void Start()
+        {
+            _stream = await whisper.CreateStream(microphoneRecord);
+            _stream.OnResultUpdated += OnResult;
+            _stream.OnSegmentUpdated += OnSegmentUpdated;
+            _stream.OnSegmentFinished += OnSegmentFinished;
+            _stream.OnStreamFinished += OnFinished;
+
+            microphoneRecord.OnRecordStop += OnRecordStop;
+            button.onClick.AddListener(OnButtonPressed);
+        }
+
+        private void OnButtonPressed()
+        {
+            if (!microphoneRecord.IsRecording)
+            {
+                _stream.StartStream();
+                microphoneRecord.StartRecord();
+            }
+            else
+                microphoneRecord.StopRecord();
+        
+            buttonText.text = microphoneRecord.IsRecording ? "Stop" : "Record";
+        }
+    
+        private void OnRecordStop(float[] data, int frequency, int channels, float length)
+        {
+            buttonText.text = "Record";
+        }
+    
+        private void OnResult(string result)
+        {
+            text.text = result;
+            UiUtils.ScrollDown(scroll);
+        }
+        
+        private void OnSegmentUpdated(WhisperResult segment)
+        {
+            print($"Segment updated: {segment.Result}");
+        }
+        
+        private void OnSegmentFinished(WhisperResult segment)
+        {
+            print($"Segment finished: {segment.Result}");
+        }
+        
+        private void OnFinished(string finalResult)
+        {
+            print("Stream finished!");
+        }
+    }
+}
